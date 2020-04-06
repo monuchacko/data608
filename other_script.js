610e1733078f497a061a2b526536c8c08b5a5f37
Monu Chacko <monuchacko@hotmail.com>
2020-04-05 22:47:33 -0400

    data 608

--- other_script.js
+++ other_script.js
@@ -1,12 +1,44 @@
 'use strict';
 
-function myScript(){
-    return('you can also import other scripts!');
+function myScript() {
+    return ('you can also import other scripts!');
 }
 
-$( document ).ready(function() {
+function reverseString(str) {
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
+}
 
-    $(btnReverse).on('click', function(){
-        $(lblReverseResult).text($(txtReverse).val());
+$(document).ready(function () {
+
+    $('#btnReverse').on('click', function () {
+        $('#lblReverseResult').text(reverseString($('#txtReverse').val()));
+    })
+
+    $('#btnMultiples').on('click', function () {
+        var html = '<table>';
+
+        var i;
+        var j;
+        var mult = $('#txtMultiples').val();
+        var incr = 1;
+        
+        debugger
+
+        for (i = 0; i < 5; i++) {
+            html += '<tr>';
+            for (j = 0; j < 4; j++) {
+                // mult = mult * incr;
+                html += '<td>' + mult * incr + '</td>';
+                incr++;
+            }
+            html += '</tr>';
+
+        }
+
+        html += '<table>';
+        $('#resMultiples').html(html);
     })
 });
